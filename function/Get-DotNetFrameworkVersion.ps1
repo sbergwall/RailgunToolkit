c711cc6b1e5c9827d7762c51154712f94afbf80f
unknown <simonbergwall@gmail.com>
2019-05-31 15:51:01 +0200

    update to dotnet function

--- function/Get-DotNetFrameworkVersion.ps1
+++ function/Get-DotNetFrameworkVersion.ps1
@@ -30,7 +30,7 @@ function Get-DotNetFrameworkVersion {
         [string[]]$Computer = $env:COMPUTERNAME
     )
 
-    begin {}
+    begin { }
 
     process {
         $ScriptBlockToRun = {
@@ -39,23 +39,23 @@ function Get-DotNetFrameworkVersion {
                 foreach ($NetRegKey in $NetRegKeys) {
                     $Release = $NetRegKey.GetValue("Release")
                     Switch ($Release) {
-                        378389 {$NetFrameworkVersion = "4.5"}
-                        378675 {$NetFrameworkVersion = "4.5.1"}
-                        378758 {$NetFrameworkVersion = "4.5.1"}
-                        379893 {$NetFrameworkVersion = "4.5.2"}
-                        393295 {$NetFrameworkVersion = "4.6"}
-                        393297 {$NetFrameworkVersion = "4.6"}
-                        394254 {$NetFrameworkVersion = "4.6.1"}
-                        394271 {$NetFrameworkVersion = "4.6.1"}
-                        394802 {$NetFrameworkVersion = "4.6.2"}
-                        394806 {$NetFrameworkVersion = "4.6.2"}
-                        460798 {$NetFrameworkVersion = "4.7"}
-                        460805 {$NetFrameworkVersion = "4.7"}
-                        461308 {$NetFrameworkVersion = "4.7.1"}
-                        461310 {$NetFrameworkVersion = "4.7.1"}
-                        461808 {$NetFrameworkVersion = "4.7.2"}
-                        461814 {$NetFrameworkVersion = "4.7.2"}
-                        Default {$NetFrameworkVersion = "Net Framework 4.5 or later is not installed."}
+                        378389 { $NetFrameworkVersion = "4.5" }
+                        378675 { $NetFrameworkVersion = "4.5.1" }
+                        378758 { $NetFrameworkVersion = "4.5.1" }
+                        379893 { $NetFrameworkVersion = "4.5.2" }
+                        393295 { $NetFrameworkVersion = "4.6" }
+                        393297 { $NetFrameworkVersion = "4.6" }
+                        394254 { $NetFrameworkVersion = "4.6.1" }
+                        394271 { $NetFrameworkVersion = "4.6.1" }
+                        394802 { $NetFrameworkVersion = "4.6.2" }
+                        394806 { $NetFrameworkVersion = "4.6.2" }
+                        460798 { $NetFrameworkVersion = "4.7" }
+                        460805 { $NetFrameworkVersion = "4.7" }
+                        461308 { $NetFrameworkVersion = "4.7.1" }
+                        461310 { $NetFrameworkVersion = "4.7.1" }
+                        461808 { $NetFrameworkVersion = "4.7.2" }
+                        461814 { $NetFrameworkVersion = "4.7.2" }
+                        Default { $NetFrameworkVersion = "Net Framework 4.5 or later is not installed." }
                     }
                     $Object = [PSCustomObject]@{
                         Computername        = $env:COMPUTERNAME
@@ -71,16 +71,11 @@ function Get-DotNetFrameworkVersion {
         }
 
         try {
-            If ($Computer -eq $env:COMPUTERNAME) {
-                Invoke-Command -ScriptBlock $ScriptBlockToRun
-            } 
-            else {
-                $Session = New-PSSession $Computer
-                Invoke-Command -Session $Session -ScriptBlock $ScriptBlockToRun
-            }
+            $Session = New-PSSession $Computer
+            Invoke-Command -Session $Session -ScriptBlock $ScriptBlockToRun
         }
         catch {
-            Write-Error -Message $_
+            $PSCmdlet.WriteError($PSItem)
         }
     }
     end {

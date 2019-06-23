9121701a3187174842bc9b3392f75d393bd69352
unknown <simonbergwall@gmail.com>
2019-06-23 22:57:48 +0200

    changes to invoke netvoew

--- function/Invoke-NetView.ps1
+++ function/Invoke-NetView.ps1
@@ -22,15 +22,15 @@ function Invoke-NetView {
     [CmdletBinding()]
 
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string]$ComputerName 
-        ,
+        [Parameter(ValueFromPipeline = $true)]
+        [string]$ComputerName = "localhost",
+
         [Parameter(Mandatory = $false)]
-        [switch]$GetItem
-        ,
+        [switch]$GetItem,
+
         [Parameter(Mandatory = $false)]
-        [string[]]$ColumnHeadings = @('Share name','Type','Used as','Comment')  #I suspect these differ depending on OS language?  Therefore made customisable
-        ,
+        [string[]]$ColumnHeadings = @('Share name', 'Type', 'Used as', 'Comment'), #I suspect these differ depending on OS language?  Therefore made customisable
+        
         [Parameter(Mandatory = $false)]
         [string]$ShareName = 'Share name' #tell us which of the properties relates to the share name
         #,
@@ -39,28 +39,34 @@ function Invoke-NetView {
     )
 
     begin {
-        [psobject[]]$Splitter = $ColumnHeadings | ForEach-Object{
+
+        if (-not (Get-Command -Name net.exe -ErrorAction SilentlyContinue)) {
+            Write-Warning -Message "Action failed because net.exe could not be found."
+            break
+        }
+
+        [psobject[]]$Splitter = $ColumnHeadings | ForEach-Object {
             $ColumnHeading = $_
             $obj = new-object -TypeName PSObject -Property @{
-                Name = $ColumnHeading
+                Name       = $ColumnHeading
                 StartIndex = 0
-                Length = 0
+                Length     = 0
             }
             $obj | Add-Member -Name Initialise -MemberType ScriptMethod {
                 param([string]$header)
                 process {
                     $_.StartIndex = $header.indexOf($_.Name)
-                    $_.Length = ($header -replace ".*($($_.Name)\s*).*",'$1').Length
+                    $_.Length = ($header -replace ".*($($_.Name)\s*).*", '$1').Length
                 }
             }
             $obj | Add-Member -Name GetValue -MemberType ScriptMethod {
                 param([string]$line)
                 process {
-                    $line -replace ".{$($_.StartIndex)}(.{$($_.Length)}).*",'$1'
+                    $line -replace ".{$($_.StartIndex)}(.{$($_.Length)}).*", '$1'
                 }
             }
             $obj | Add-Member -Name Process -MemberType ScriptMethod {
-                param([psobject]$obj,[string]$line)
+                param([psobject]$obj, [string]$line)
                 process {
                     $obj | Add-Member -Name $_.Name -MemberType NoteProperty -Value ($_.GetValue($line))
                 }
@@ -72,18 +78,24 @@ function Invoke-NetView {
     process {
 
         [string[]]$output = (NET.EXE VIEW $ComputerName)
-        [string]$headers = $output[4] #find the data's heading row
-        $output = $output[7..($output.Length-3)] #keep only the data rows
-        $Splitter | %{$_.Initialise($headers)}
+        If ($LASTEXITCODE -eq 0) {
 
-        foreach($line in $output) { 
-            [psobject]$result = new-object -TypeName PSObject -Property @{ComputerName=$ComputerName;}
-            $Splitter | %{$_.Process($result,$line)}
-            $result | Add-Member '_ShareNameColumnName' -MemberType NoteProperty -Value $ShareName
-            $result | Add-Member 'Path' -MemberType ScriptProperty -Value {("\\{0}\{1}" -f $this.ComputerName,$this."$($this._ShareNameColumnName)")}
-            $result | Add-Member 'Item' -MemberType ScriptProperty -Value {Get-Item ($this.Path)}
-            $result | Add-Member -MemberType MemberSet -Name PSStandardMembers -Value ([System.Management.Automation.PSMemberInfo[]]@(New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]](@('ComputerName','Path') + $ColumnHeadings))))
-            $result
+            [string]$headers = $output[4] #find the data's heading row
+            $output = $output[7..($output.Length - 3)] #keep only the data rows
+            $Splitter | % { $_.Initialise($headers) }
+
+            foreach ($line in $output) { 
+                [psobject]$result = new-object -TypeName PSObject -Property @{ComputerName = $ComputerName; }
+                $Splitter | % { $_.Process($result, $line) }
+                $result | Add-Member '_ShareNameColumnName' -MemberType NoteProperty -Value $ShareName
+                $result | Add-Member 'Path' -MemberType ScriptProperty -Value { ("\\{0}\{1}" -f $this.ComputerName, $this."$($this._ShareNameColumnName)") }
+                $result | Add-Member 'Item' -MemberType ScriptProperty -Value { Get-Item ($this.Path) }
+                $result | Add-Member -MemberType MemberSet -Name PSStandardMembers -Value ([System.Management.Automation.PSMemberInfo[]]@(New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’, [string[]](@('ComputerName', 'Path') + $ColumnHeadings))))
+                $result
+            }
+        }
+        else {
+            throw "Lastexitcode: $LASTEXITCODE"
         }
     }
 }

4e52f8bfda08d2901911c33b7c0833afad5c40a6
Simon <simonbergwall@gmail.com>
2020-01-17 17:31:36 +0100

    fixed bug in property param and added filter param

--- function/Get-ADUserLastLogon.ps1
+++ function/Get-ADUserLastLogon.ps1
@@ -11,12 +11,16 @@ function Get-ADUserLastLogon {
 
         [Parameter(Position = 1)]
         [ValidateNotNullOrEmpty()]
-        [String[]]$Property
+        [String[]]$Property,
+
+        [Parameter(Position = 2)]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$Filter
     )
 
     begin {
         Write-Verbose -Message "[BEGIN  ] Starting: $($MyInvocation.Mycommand)"
-		
+       
         Write-Verbose -Message "[BEGIN  ] Finding All Domain Controllers"
         $ADDomainControllers = Get-ADDomainController -Filter { Name -like "*" }
         $NumberofDCs = ($ADDomainControllers | Measure-Object).count
@@ -27,14 +31,19 @@ function Get-ADUserLastLogon {
         If (-not($PSBoundParameters.ContainsKey('SamAccountName'))) {
             try {
                 Write-Verbose "[BEGIN  ] No SamAccountName was specified, getting all AD user accounts in domain"
-                $SamAccountName = Get-ADUser -Filter * -Properties $Property -ErrorAction Stop
+                $adSplat = @{
+                    Filter      = If ($Filter) { $Filter } else { "*" }
+                    Property    = If ($Property) { $Property } else { "*" }
+                    ErrorAction = "Stop"
+                }
+
+                $SamAccountName = Get-ADUser @adSplat
             } 
             catch {
                 $PSCmdlet.WriteError($psitem) 
             }
         }
     } # Begin
-
 
     process {
         foreach ($user in $SamAccountName ) {
@@ -44,7 +53,7 @@ function Get-ADUserLastLogon {
                         $Splatting = @{
                             Identity    = $user 
                             ErrorAction = "Stop"
-                            
+                           
                         }
                         If (-not($null -eq $Property)) {
                             $Splatting.Properties = $Property
@@ -52,11 +61,12 @@ function Get-ADUserLastLogon {
 
                         $user = Get-ADUser @Splatting
                     }
-                
+               
                     { $user -is [Microsoft.ActiveDirectory.Management.ADUser] } {
                         If ($PSBoundParameters.ContainsKey('Property')) { $user = Get-ADUser -Identity $user -Properties $Property }
                     }
                     default { throw "Unknown type was used." }
+
                 } # Switch
             }  
             catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
@@ -70,7 +80,7 @@ function Get-ADUserLastLogon {
             }
 
             $lastlogon = 0
-        
+       
             foreach ($ADDomainController in $ADDomainControllers) {
                 $DCHostname = $ADDomainController.HostName
                 Write-Verbose -Message "[Process] Checking on Domain Controller: $DCHostname"
@@ -81,16 +91,17 @@ function Get-ADUserLastLogon {
                         ErrorAction = 'Stop'
                         Properties  = 'lastLogon'
                     }
-                
+               
                     $ADUser = Get-ADUser @paramGetADUser
                 } #Try
                 Catch {
                     Write-Warning "Error on:$DCHostname. Message: $psitem"
                 }
+
                 Write-Verbose -Message "[Process]`t Converting To DateTime"
                 $ADUserlastLogon = [DateTime]::FromFileTime($ADUser.lastLogon)
+
                 Write-Verbose -Message "[Process]`t LastLogon Date: $ADUserlastLogon"
-            
                 if ($ADUserLastLogon -gt $lastLogon) {
                     $lastLogon = $ADUserlastLogon
                     $OwningDC = $DCHostname
